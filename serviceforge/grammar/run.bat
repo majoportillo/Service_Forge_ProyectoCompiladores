58b53d3849ff69d68352d04ece8a421321f3aeda
mlyyppol <apenap1@miumg.edu.gt>
2026-05-26 21:14:28 -0600

    Limpieza de archivos

--- serviceforge/grammar/run.bat
+++
@@ -1,3 +0,0 @@
-java -jar C:\Users\Usuario\Documents\antlr\antlr-4.13.2-complete.jar ServiceForge.g4
-javac -cp ".;C:\Users\Usuario\Documents\antlr\antlr-4.13.2-complete.jar" *.java
-java -cp ".;C:\Users\Usuario\Documents\antlr\antlr-4.13.2-complete.jar" org.antlr.v4.gui.TestRig ServiceForge program -gui prueba.svc
