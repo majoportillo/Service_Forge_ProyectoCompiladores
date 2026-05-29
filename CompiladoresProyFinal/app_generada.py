fd62be71d929cfa14d8910d36fc6cfe539def0ed
ErgilC1780 <ergilcruz@gmail.com>
2026-05-29 09:53:46 -0600

    Primer archivo CRUD usuarios.svc

--- CompiladoresProyFinal/app_generada.py
+++ CompiladoresProyFinal/app_generada.py
@@ -1,17 +1,25 @@
 from flask import Flask, jsonify, request
-app = Flask('MiApiIncreible')
+app = Flask('UserManagement')
 
-@app.route('/api/listar', methods=['GET'])
-def get__listar():
-    return jsonify({'message': 'Endpoint /api/listar ejecutado con exito'}), 200
+@app.route('/api/v1/users', methods=['GET'])
+def get__users():
+    return jsonify({'message': 'Endpoint /api/v1/users ejecutado con exito'}), 200
 
-@app.route('/api/crear', methods=['POST'])
-def post__crear():
-    return jsonify({'message': 'Endpoint /api/crear ejecutado con exito'}), 200
+@app.route('/api/v1/users', methods=['POST'])
+def post__users():
+    return jsonify({'message': 'Endpoint /api/v1/users ejecutado con exito'}), 200
 
-@app.route('/api/buscar/<id>', methods=['GET'])
-def get__buscar_id():
-    return jsonify({'message': 'Endpoint /api/buscar/<id> ejecutado con exito'}), 200
+@app.route('/api/v1/users/<id>', methods=['GET'])
+def get__users_id():
+    return jsonify({'message': 'Endpoint /api/v1/users/<id> ejecutado con exito'}), 200
+
+@app.route('/api/v1/users/<id>', methods=['PUT'])
+def put__users_id():
+    return jsonify({'message': 'Endpoint /api/v1/users/<id> ejecutado con exito'}), 200
+
+@app.route('/api/v1/users/<id>', methods=['DELETE'])
+def delete__users_id():
+    return jsonify({'message': 'Endpoint /api/v1/users/<id> ejecutado con exito'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
